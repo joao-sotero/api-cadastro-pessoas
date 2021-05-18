73eb12d7686b4c1e16e06a7c8cf54b16700b880c
joao-sotero <vitor99jjunior@gmail.com>
2021-05-18 14:21:12 -0300

    Developed test unitary

---
+++ src/test/java/com/api/cadastro/usuario/personapi/utils/PhoneUtils.java
@@ -0,0 +1,28 @@
+package com.api.cadastro.usuario.personapi.utils;
+
+
+import com.api.cadastro.usuario.personapi.dto.request.PhoneDTO;
+import com.api.cadastro.usuario.personapi.entity.Phone;
+import com.api.cadastro.usuario.personapi.enums.PhoneType;
+
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "1199999-9999";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
